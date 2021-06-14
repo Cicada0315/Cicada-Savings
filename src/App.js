cc5112917275568a9b011d7ec8bfb29683c118a3
jean <jin.ahn.1027@gmail.com>
2021-06-14 14:55:05 -0400

    create main nevbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <Container>
+      <div className="App">
+        <NavBar />
+        <h1>Hello world</h1>
+        
+      </div>
+      </Container>
+    );
+  }
+};
 
 export default App;
