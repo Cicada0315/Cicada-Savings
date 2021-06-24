dd0ea7f4a8026cc8d442e4e5b43a69931784f2fb
jean <jin.ahn.1027@gmail.com>
2021-06-24 15:56:54 -0400

    modified NavBar.js Nav.Links look better

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,24 @@ import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
 const NavBar=()=>{
     return(
-        <Navbar bg="light" expand="lg">
         <Container>
+            <Navbar bg="light" expand="lg">
             <Navbar.Brand href="/"><img src={Cicada} width="30" height="30" alt="logo"/> CicadaSavings</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="/checkings">Checking</Nav.Link>
-                <Nav.Link href="/savings">Savings</Nav.Link>
-                <Nav.Link href="/login"><Button className="buttons" variant="primary">Log In</Button></Nav.Link>
-            </Nav>
+                <Nav className="me-auto">
+                    <Nav.Link href="/checkings">Checking</Nav.Link>
+                    <Nav.Link href="/savings">Savings</Nav.Link>
+                </Nav>
+
+                <Nav>
+                    <Navbar.Text>
+                    <Nav.Link href="/login"><Button className="buttons" variant="primary">Log In</Button></Nav.Link>
+                    </Navbar.Text>
+                </Nav>
             </Navbar.Collapse>
-        </Container>
-        </Navbar>       
+            </Navbar> 
+        </Container>   
     )
 }
 
